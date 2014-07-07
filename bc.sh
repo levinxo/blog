cde16e30c6a1a330833547b1e715d0c38b9bfa95
levin <levinxo@gmail.com>
2014-07-07 23:06:59 +0800

    this shell script is used for adding disqus_identifier to all posts

---
+++ bc.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#add disqus_identifier to posts
+for f in `ls content/2*`;
+do
+    head -n 7 $f | grep "disqus_identifier: "
+    if [ $? -eq 1 ];then
+        date_ori=`head -n 6 $f | grep "Date: "`
+        date=${date_ori#Date: }
+        date=`echo $date | sed -e 's/-//g' -e 's/ //g' -e 's/://g'`
+        
+        sed "s/Date: .*/${date_ori}newlinelinenewdisqus_identifier: ${date}/" $f | sed 's/newlinelinenew/\
+/' > $f.new
+        mv $f.new $f
+    fi
+done
+
+
