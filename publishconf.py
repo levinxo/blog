be15bc31a37647dcb87afbc6d2bd034b4598a6ee
levin <levinxo@gmail.com>
2015-02-18 22:00:35 +0800

    改变url的scheme为https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.bicky.me'
+SITEURL = 'https://www.bicky.me'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
