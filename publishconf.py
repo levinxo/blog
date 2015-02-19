a5e95326231dc2dc182d73b613c426ec18e6bdde
levin <levinxo@gmail.com>
2015-02-19 15:59:03 +0800

    将绝对URL改为相对URL，可同时支持http和https访问

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'https://www.bicky.me'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
